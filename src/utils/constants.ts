770faba99074015c5422ad13725515820824e88d
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-06 18:32:35 -0400

    Update URLs again

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,5 +3,5 @@
 // export const CLIENT_URL = 'http://localhost:5173'
 
 // For production use
-export const SERVER_URL = 'https://tictactoemo-production.up.railway.app/'
-export const CLIENT_URL = 'https://tictactoemo-git-deploybranch-harrisons-projects-baa0d10b.vercel.app/'
+export const SERVER_URL = 'https://tictactoemo-production.up.railway.app'
+export const CLIENT_URL = 'https://tictactoemo.vercel.app'
